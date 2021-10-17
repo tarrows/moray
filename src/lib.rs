908f00ab9bdfc56084d25d60df722cf205d39f80
tarrows <rem_tate@yahoo.co.jp>
2021-10-17 22:00:08 +0900

    start smaller w/wasm-pack

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,16 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
-extern "C" {
-  fn alert(s: &str);
+pub fn greet() -> String {
+  "Hello, {{project-name}}!".to_string()
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-  alert("Hello, {{project-name}}!");
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn it_works() {
+    assert_eq!(greet(), "Hello, {{project-name}}!".to_string())
+  }
 }
