8ff1d7ece31b664929698c27b27a20dff7d95821
tarrows <rem_tate@yahoo.co.jp>
2021-10-17 22:11:35 +0900

    setup webpack project

---
+++ web/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/index.html")
+    })
+  ]
+};
